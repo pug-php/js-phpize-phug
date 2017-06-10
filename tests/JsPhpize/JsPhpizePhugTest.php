e5a6b9bdabbd66fa38a4c0fa6c25f47161252dc6
KyleKatarn <kylekatarnls@gmail.com>
2017-06-10 22:15:28 +0200

    Add tests

--- tests/JsPhpize/JsPhpizePhugTest.php
+++ tests/JsPhpize/JsPhpizePhugTest.php
@@ -17,5 +17,15 @@ class JsPhpizePhugTest extends \PHPUnit_Framework_TestCase
             '<a data-foo="<?= htmlspecialchars((is_array($_pug_temp = array( \'message\' => "Hello" )) || (is_object($_pug_temp) && !method_exists($_pug_temp, "__toString")) ? json_encode($_pug_temp) : strval($_pug_temp))) ?>"></a>',
             $compiler->compile('a(data-foo={message: "Hello"})')
         );
+
+        self::assertSame(
+            '<a foo="<?= (is_array($_pug_temp = $foo) || (is_object($_pug_temp) && !method_exists($_pug_temp, "__toString")) ? json_encode($_pug_temp) : strval($_pug_temp)) ?>"></a>',
+            $compiler->compile('a(foo?!=foo)')
+        );
+
+        self::assertSame(
+            '<a foo="<?= (is_array($_pug_temp = array("foo" => "bar")[\'foo\']) || (is_object($_pug_temp) && !method_exists($_pug_temp, "__toString")) ? json_encode($_pug_temp) : strval($_pug_temp)) ?>"></a>',
+            $compiler->compile('a(foo?!=array("foo" => "bar")[\'foo\'])')
+        );
     }
 }
