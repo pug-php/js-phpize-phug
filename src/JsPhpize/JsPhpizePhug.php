39cd02b35f95653032b529386dc1d5d18b3ab894
KyleKatarn <kylekatarnls@gmail.com>
2017-06-18 15:32:12 +0200

    Optimize output and avoid split if-else structures

--- src/JsPhpize/JsPhpizePhug.php
+++ src/JsPhpize/JsPhpizePhug.php
@@ -35,6 +35,18 @@ class JsPhpizePhug extends CompilerModule
         $compiler->addHook('post_compile', 'jsphpize', function ($phpCode) use (&$compiler) {
             /** @var JsPhpize $jsPhpize */
             $jsPhpize = $compiler->getOption('jsphpize_engine');
+            $phpCode = preg_replace(
+                '/\{\s*\?><\?(?:php)?\s*\}/',
+                '{}',
+                $phpCode
+            );
+            $phpCode = preg_replace(
+                '/\}\s*\?><\?(?:php)?\s*('.
+                'else(if)?|for|while|switch|function'.
+                ')(?![a-zA-Z0-9_])/',
+                '} $1',
+                $phpCode
+            );
             $dependencies = $jsPhpize->compileDependencies();
             if ($dependencies !== '') {
                 $phpCode = $compiler->getFormatter()->handleCode($dependencies) . $phpCode;
