4da1ca3192fd45a9b702ae823bbd6c9e31bea3f4
KyleKatarn <kylekatarnls@gmail.com>
2018-11-07 17:54:28 +0100

    Isolate compilation stuff into a trait

--- src/JsPhpize/JsPhpizePhug.php
+++ src/JsPhpize/JsPhpizePhug.php
@@ -3,14 +3,11 @@
 namespace JsPhpize;
 
 use Exception;
-use JsPhpize\Compiler\Exception as CompilerException;
-use JsPhpize\Lexer\Exception as LexerException;
-use JsPhpize\Parser\Exception as ParserException;
+use JsPhpize\Traits\Compilation;
 use Phug\AbstractCompilerModule;
 use Phug\Compiler;
 use Phug\Compiler\Event\NodeEvent;
 use Phug\CompilerEvent;
-use Phug\CompilerInterface;
 use Phug\Formatter\Element\KeywordElement;
 use Phug\Parser\Node\CommentNode;
 use Phug\Parser\Node\KeywordNode;
@@ -20,6 +17,8 @@ use Phug\Util\ModuleContainerInterface;
 
 class JsPhpizePhug extends AbstractCompilerModule
 {
+    use Compilation;
+
     protected $languages = ['js', 'php'];
 
     public function __construct(ModuleContainerInterface $container)
@@ -74,6 +73,7 @@ class JsPhpizePhug extends AbstractCompilerModule
 
     public function handleNodeEvent(NodeEvent $event)
     {
+        /* @var CommentNode $node */
         if (($node = $event->getNode()) instanceof CommentNode &&
             !$node->isVisible() &&
             $node->hasChildAt(0) &&
@@ -170,102 +170,5 @@ class JsPhpizePhug extends AbstractCompilerModule
             is_array($tokens[$index - 3]) &&
             $tokens[$index - 3][0] === T_CONSTANT_ENCAPSED_STRING &&
             preg_match('/_with_ref\'$/', $tokens[$index - 3][1]);
-    }
-
-    /**
-     * @return JsPhpize
-     */
-    public function getJsPhpizeEngine(CompilerInterface $compiler)
-    {
-        if (!$compiler->hasOption('jsphpize_engine')) {
-            $compiler->setOption('jsphpize_engine', new JsPhpize($this->getOptions()));
-        }
-
-        return $compiler->getOption('jsphpize_engine');
-    }
-
-    /**
-     * @param JsPhpize $jsPhpize
-     * @param int      $code
-     * @param string   $fileName
-     *
-     * @throws Exception
-     *
-     * @return Exception|string
-     */
-    public function compile(JsPhpize $jsPhpize, $code, $fileName)
-    {
-        try {
-            $phpCode = trim($jsPhpize->compile($code, $fileName ?: 'raw string'));
-            $phpCode = preg_replace('/\{\s*\}$/', '', $phpCode);
-            $phpCode = preg_replace(
-                '/^(?<!\$)\$+(\$[a-zA-Z\\\\\\x7f-\\xff][a-zA-Z0-9\\\\_\\x7f-\\xff]*\s*[=;])/',
-                '$1',
-                $phpCode
-            );
-
-            return rtrim(trim($phpCode), ';');
-        } catch (Exception $exception) {
-            if ($exception instanceof LexerException ||
-                $exception instanceof ParserException ||
-                $exception instanceof CompilerException
-            ) {
-                return $exception;
-            }
-
-            throw $exception;
-        }
-    }
-
-    /**
-     * @param CompilerInterface $compiler
-     * @param string            $output
-     *
-     * @return string
-     */
-    protected function parseOutput($compiler, $output)
-    {
-        $jsPhpize = $this->getJsPhpizeEngine($compiler);
-        $output = preg_replace(
-            '/\{\s*\?><\?(?:php)?\s*\}/',
-            '{}',
-            $output
-        );
-        $output = preg_replace(
-            '/\}\s*\?><\?(?:php)?\s*(' .
-            'else(if)?|for|while|switch|function' .
-            ')(?![a-zA-Z0-9_])/',
-            '} $1',
-            $output
-        );
-
-        $dependencies = $jsPhpize->compileDependencies();
-        if ($dependencies !== '') {
-            $output = $compiler->getFormatter()->handleCode($dependencies) . $output;
-        }
-
-        $jsPhpize->flushDependencies();
-
-        return $output;
-    }
-
-    public function handleOutputEvent(Compiler\Event\OutputEvent $event)
-    {
-        /** @var CompilerInterface $compiler */
-        $compiler = $event->getTarget();
-
-        $event->setOutput($this->parseOutput($compiler, $event->getOutput()));
-
-        $compiler->unsetOption('jsphpize_engine');
-    }
-
-    /**
-     * @return array
-     */
-    public function getEventListeners()
-    {
-        return [
-            CompilerEvent::OUTPUT => [$this, 'handleOutputEvent'],
-        ];
     }
 }
